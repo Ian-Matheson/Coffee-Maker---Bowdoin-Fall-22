e8341f8ed97ac859b2151ab3cf68e724798bc455
Ian Matheson <imatheso@bowdoin.edu>
2022-10-30 21:38:58 -0400

    [Testing] Creating tests for invalid delete recipes and invalid recipes. Also implemented a BeforeEach method.

--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/TestDatabaseInteraction.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/TestDatabaseInteraction.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +29,19 @@ public class TestDatabaseInteraction {
 	@Autowired
 	private RecipeService recipeService;
 	
+	
+	/**
+	 * BeforeEach clears the current Recipe Service.
+	 * @throws Exception if fails to build
+	 */
+	@BeforeEach
+	public void setUp() throws Exception {
+		recipeService.deleteAll();
+	}
+	
 	@Test
 	@Transactional
-	public void testRecipes(){
-		recipeService.deleteAll();
+	public void testValidRecipes(){
 		
 		Recipe r = new Recipe();
 	    
@@ -75,8 +86,123 @@ public class TestDatabaseInteraction {
 	
 	@Test
 	@Transactional
+	public void testInvalidRecipes(){
+		
+		Recipe r1 = new Recipe();
+		r1.setName("mocha");
+	    r1.setPrice(50);
+	    r1.setMilk(1);
+	    r1.setSugar(1);
+	    r1.setCoffee(2);
+	    r1.setChocolate(1);
+	    recipeService.save( r1 );
+	    
+	    Recipe r2 = new Recipe();
+		r2.setName("latte");
+	    r2.setPrice(50);
+	    r2.setMilk(3);
+	    r2.setSugar(1);
+	    r2.setCoffee(3);
+	    r2.setChocolate(0);
+	    recipeService.save( r2 );
+	    
+	    //testing setting a duplicate recipe
+	    try {
+	    	Recipe r3 = new Recipe();
+			r3.setName("latte");
+		    r3.setPrice(50);
+		    r3.setMilk(3);
+		    r3.setSugar(1);
+		    r3.setCoffee(3);
+		    r3.setChocolate(0);
+		    recipeService.save( r3 );
+		    Assertions.fail("Adding a duplicate recipe to a list should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+		    Assertions.assertEquals( 2, recipeService.count() ); //ensuring duplicate not added
+		}
+	    
+	    Recipe r3 = new Recipe();
+		r3.setName("espresso");
+	    r3.setPrice(50);
+	    r3.setMilk(1);
+	    r3.setSugar(1);
+	    r3.setCoffee(5);
+	    r3.setChocolate(1);
+	    recipeService.save( r3 );
+	    
+	    //testing too many recipes
+	    try {
+	    	Recipe r4 = new Recipe();
+			r4.setName("black");
+		    r4.setPrice(20);
+		    r4.setMilk(0);
+		    r4.setSugar(0);
+		    r4.setCoffee(3);
+		    r4.setChocolate(0);
+		    recipeService.save( r4 );
+		    Assertions.fail("Adding a fourth recipe to a list should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+	    	Assertions.assertEquals( 3, recipeService.count() ); //ensuring fourth recipe not added
+		}
+	    
+	    recipeService.deleteAll();
+	    
+	    Recipe r = new Recipe();
+	    
+		//testing setName
+		try {
+			r.setName(null);
+			Assertions.fail("Setting name to null should throw iae but did not");
+		} catch (IllegalArgumentException iae) {
+		}
+		
+		try {
+			r.setName("");
+			Assertions.fail("Setting name to empty string should throw iae but did not");
+		} catch (IllegalArgumentException iae) {
+		}
+	
+		//testing set price
+	    try {
+	    	r.setPrice(-1);
+	    	Assertions.fail("Setting price to a negative number should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+		}
+	    
+	    //testing set milk
+	    try {
+	    	r.setMilk(-1);
+	    	Assertions.fail("Setting milk to a negative number should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+		}
+	    
+	    //testing set sugar
+	    try {
+	    	r.setSugar(-1);
+	    	Assertions.fail("Setting milk to a negative number should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+		}
+	    
+	    //testing set coffee
+	    try {
+	    	r.setCoffee(-1);
+	    	Assertions.fail("Setting coffee to a negative number should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+		}
+	    
+	    //testing set chocolate
+	    try {
+	    	r.setChocolate(-1);
+	    	Assertions.fail("Setting chocolate to a negative number should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+		}
+	}
+	
+	
+	
+	@Test
+	@Transactional
 	public void testValidDeleteRecipes(){
-		recipeService.deleteAll();
 		
 		Recipe r = new Recipe();
 	    
@@ -106,28 +232,68 @@ public class TestDatabaseInteraction {
 	    Recipe dbRecipe2 = dbRecipes.get(1);
 	    
 	    recipeService.delete(dbRecipe2);
+	    
+	    List<Recipe> dbRecipes2 = (List<Recipe>) recipeService.findAll();
 
-	    assertEquals(1, dbRecipes.size());
+	    assertEquals(1, dbRecipes2.size());
 
-	    assertEquals(r.getName(), dbRecipe.getName());
-	    assertEquals(r.getPrice(), dbRecipe.getPrice());
-	    assertEquals(r.getMilk(), dbRecipe.getMilk());
-	    assertEquals(r.getSugar(), dbRecipe.getSugar());
-	    assertEquals(r.getCoffee(), dbRecipe.getCoffee());
-	    assertEquals(r.getChocolate(), dbRecipe.getChocolate());
+	    assertEquals(r.getName(), dbRecipe2.getName());
+	    assertEquals(r.getPrice(), dbRecipe2.getPrice());
+	    assertEquals(r.getMilk(), dbRecipe2.getMilk());
+	    assertEquals(r.getSugar(), dbRecipe2.getSugar());
+	    assertEquals(r.getCoffee(), dbRecipe2.getCoffee());
+	    assertEquals(r.getChocolate(), dbRecipe2.getChocolate());
 	    
 	    recipeService.delete(dbRecipe);
 	    
-	    assertEquals(0, dbRecipes.size());
+	    List<Recipe> dbRecipes3 = (List<Recipe>) recipeService.findAll();
+	    
+	    assertEquals(0, dbRecipes3.size());
 
-  
 	}
 	
 	@Test
 	@Transactional
 	public void testInvalidDeleteRecipes(){
-		//TODO
+
+		//testing deleting a ticket that has already been deleted
+	    try {
+			Recipe r1 = new Recipe();
+			r1.setName("mocha");
+		    r1.setPrice(50);
+		    r1.setMilk(1);
+		    r1.setSugar(1);
+		    r1.setCoffee(2);
+		    r1.setChocolate(1);
+		    recipeService.save( r1 );
+		    
+		    recipeService.delete( r1 );
+		    
+		    Assertions.assertEquals( 0, recipeService.count() );
+	    	recipeService.delete( r1 );
+	    	Assertions.fail("Deleting a recipe that has already been deleted from delete (DNE) should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+	    	Assertions.assertEquals( 0, recipeService.count() );
+		}
+	    
+	    //testing deleting a ticket that has already been deleted
+	    try {
+			Recipe r2 = new Recipe();
+			r2.setName("black");
+		    r2.setPrice(20);
+		    r2.setMilk(0);
+		    r2.setSugar(0);
+		    r2.setCoffee(3);
+		    r2.setChocolate(0);
+		    recipeService.save( r2 );
+		    
+		    recipeService.deleteAll();
+		    
+		    Assertions.assertEquals( 0, recipeService.count() );
+	    	recipeService.delete( r2 );
+	    	Assertions.fail("Deleting a recipe that has already been deleted from deleteAll (DNE) should throw iae but did not");
+	    } catch (IllegalArgumentException iae) {
+	    	Assertions.assertEquals( 0, recipeService.count() );
+		}
 	}
-	
-	
 }
