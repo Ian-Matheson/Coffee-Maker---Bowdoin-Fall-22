52d01e7765f25d244a0c2fc94332a0435c811a8b
John Soeder <jsoeder@bowdoin.edu>
2022-10-30 20:36:35 -0400

    [Testing] Testing valid delete recipe actions in the DB. Merge branch 'main' of https://github.com/bowdoin-csci2335-fall2022/project2-t-07.git into main

--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/TestDatabaseInteraction.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/TestDatabaseInteraction.java
@@ -70,11 +70,64 @@ public class TestDatabaseInteraction {
         assertEquals(12, dbRecipe.getSugar());
         assertEquals(4, dbRecipe.getCoffee());
         assertEquals(2, dbRecipe.getChocolate());
-        
 	       
 	}
 	
+	@Test
+	@Transactional
+	public void testValidDeleteRecipes(){
+		recipeService.deleteAll();
+		
+		Recipe r = new Recipe();
+	    
+	    r.setName("mocha");
+	    r.setPrice(50);
+	    r.setMilk(1);
+	    r.setSugar(1);
+	    r.setCoffee(2);
+	    r.setChocolate(1);
+	    recipeService.save( r );
+	    
+	    Recipe r2 = new Recipe();
+	    
+	    r2.setName("americano");
+	    r2.setPrice(50);
+	    r2.setMilk(1);
+	    r2.setSugar(1);
+	    r2.setCoffee(2);
+	    r2.setChocolate(1);
+	    recipeService.save( r2 );
+		
+	    List<Recipe> dbRecipes = (List<Recipe>) recipeService.findAll();
+
+	    assertEquals(2, dbRecipes.size());
+
+	    Recipe dbRecipe = dbRecipes.get(0);
+	    Recipe dbRecipe2 = dbRecipes.get(1);
+	    
+	    recipeService.delete(dbRecipe2);
+
+	    assertEquals(1, dbRecipes.size());
+
+	    assertEquals(r.getName(), dbRecipe.getName());
+	    assertEquals(r.getPrice(), dbRecipe.getPrice());
+	    assertEquals(r.getMilk(), dbRecipe.getMilk());
+	    assertEquals(r.getSugar(), dbRecipe.getSugar());
+	    assertEquals(r.getCoffee(), dbRecipe.getCoffee());
+	    assertEquals(r.getChocolate(), dbRecipe.getChocolate());
+	    
+	    recipeService.delete(dbRecipe);
+	    
+	    assertEquals(0, dbRecipes.size());
+
+  
+	}
 	
+	@Test
+	@Transactional
+	public void testInvalidDeleteRecipes(){
+		//TODO
+	}
 	
 	
 }
