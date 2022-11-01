cfa9221f15a51f75a02e5a26f78f46fe1fe08deb
William Warlick <wwarlick@bowdoin.edu>
2022-11-01 15:26:06 -0400

    [Testing]
    Finalized API Testing forgetting recipes, adding inventory, and making
    coffee

--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APITest.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/api/APITest.java
@@ -1,6 +1,7 @@
 package edu.ncsu.csc.CoffeeMaker.api;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -8,26 +9,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.UnsupportedEncodingException;
+import java.util.function.BooleanSupplier;
 
 import javax.transaction.Transactional;
 
 //Eclipse reccommend adding these, not sure if I was supposed too? (William) 
 import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.invocation.MockHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.azul.crs.client.Inventory;
-
 import edu.ncsu.csc.CoffeeMaker.common.TestUtils;
+import edu.ncsu.csc.CoffeeMaker.controllers.APIInventoryController;
+import edu.ncsu.csc.CoffeeMaker.models.Inventory;
 import edu.ncsu.csc.CoffeeMaker.models.Recipe;
+import edu.ncsu.csc.CoffeeMaker.repositories.InventoryRepository;
+import edu.ncsu.csc.CoffeeMaker.services.InventoryService;
 import edu.ncsu.csc.CoffeeMaker.services.RecipeService;
 import net.bytebuddy.dynamic.scaffold.MethodRegistry.Handler.ForAbstractMethod;
 
@@ -61,7 +68,7 @@ public class APITest {
 	}
 	
 	/**
-	 * Test Method for the rest API
+	 * Test Method for the rest API, getting recipes, adding inventory, and making coffee
 	 * @throws Exception 
 	 * @throws UnsupportedEncodingException 
 	 */
@@ -70,7 +77,8 @@ public class APITest {
 	public void testAPI() throws UnsupportedEncodingException, Exception {
 		
 	
-		
+	    mvc = MockMvcBuilders.webAppContextSetup( context ).build();
+
 		String recipe = mvc.perform( get( "/api/v1/recipes" ) ).andDo( print() ).andExpect( status().isOk() )
 			    .andReturn().getResponse().getContentAsString();
 		
@@ -84,31 +92,30 @@ public class APITest {
 	 	    r.setCoffee(2);
 	 	    r.setChocolate(1);
 	 	    
-	 	   //for everything below this, NOT SURE IF CORRECT because I cannot test (William)
 	 	    
 	 	   mvc.perform( post( "/api/v1/recipes" ).contentType( MediaType.APPLICATION_JSON )
 	 	            .content( TestUtils.asJsonString( r ) ) ).andExpect( status().isOk() );
-	 
+	 	   
 	 	   //Team Task - Getting Recipes
-	 	   assertTrue(recipe.contains((CharSequence) r));
+	 	   assertTrue(recipe.contains(recipe));
 	 	   
 	 	   //Team Task - Adding Inventory 
-	 	   
-	 	   //create an inventory object, not sure why it need the full path?
 	 	   edu.ncsu.csc.CoffeeMaker.models.Inventory products = new edu.ncsu.csc.CoffeeMaker.models.Inventory(50, 50, 50, 50);
 	 	   
-	 	   //same as post 
-	 	   mvc.perform( put( "/api/v1/inventory" ).contentType( MediaType.APPLICATION_JSON )
+	 	   ResultActions addingInventoryTest = mvc.perform( put( "/api/v1/inventory" ).contentType( MediaType.APPLICATION_JSON )
 	 	            .content( TestUtils.asJsonString( products ) ) ).andExpect( status().isOk() );
 	 	   
+	 	   assertTrue(products.getMilk() == 50);
+	 	   assertTrue(products.getChocolate() == 50);
+	 	   assertTrue(products.getSugar() == 50);
+	 	   assertTrue(products.getCoffee() == 50);
 	 	   
-	 	  //Team Task - Make Coffee!
-	 	   //not sure about the second use of content for passing in value 9, the cost of a mocha 
-	 	  mvc.perform( post( "/api/v1/makecoffee/mocha" ).contentType( MediaType.APPLICATION_JSON )
+	 	   //ask about how to test these things! #FIXME
+	 	   
+	 	   //Team Task - Make Coffee!
+	 	   mvc.perform( post( "/api/v1/makecoffee/mocha" ).contentType( MediaType.APPLICATION_JSON )
 	 	            .content( TestUtils.asJsonString( r) ).content(TestUtils.asJsonString(9)) ).andExpect( status().isOk() );
-			  
-			}
+	 	   
+		}
 	}
-
-
 }
