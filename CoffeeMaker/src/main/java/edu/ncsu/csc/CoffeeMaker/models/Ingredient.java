0df86767d8a68a1846e81356d60331f693586ac6
Ian Matheson <imatheso@bowdoin.edu>
2022-11-18 10:50:54 -0500

    [Implementation]
    Creating ingredient class, ingredient type enum, and removing old
    ingredient data from recipe.

---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/Ingredient.java
@@ -0,0 +1,92 @@
+package edu.ncsu.csc.CoffeeMaker.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+import edu.ncsu.csc.CoffeeMaker.models.enums.IngredientType.Ingredients;
+
+/**
+ * Inventory for the coffee maker. Inventory is tied to the database using
+ * Hibernate libraries. See InventoryRepository and InventoryService for the
+ * other two pieces used for database support.
+ *
+ * @author Kai Presler-Marshall
+ */
+@Entity
+public class Ingredient extends DomainObject {
+
+
+    /** id for ingredient entry */
+    @Id
+    @GeneratedValue
+    private Long    id;
+    
+	/** enum of available ingredients */
+    private Ingredients ingredient;
+    
+    /** amount of ingredient */
+    @Min ( 0 )
+    private Integer amount;
+    
+	public Ingredient(Ingredients ingredient, @Min(0) Integer amount) {
+		super();
+		this.ingredient = ingredient;
+		this.amount = amount;
+	}
+    
+    /**
+     * Empty constructor for Hibernate
+     */
+    public Ingredient() {
+
+    }
+    
+    /**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the ingredient
+	 */
+	public Ingredients getIngredient() {
+		return ingredient;
+	}
+
+	/**
+	 * @param ingredient the ingredient to set
+	 */
+	public void setIngredient(Ingredients ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	/**
+	 * @return the amount
+	 */
+	public Integer getAmount() {
+		return amount;
+	}
+
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	    
+	@Override
+	public String toString() {
+		return "Ingredient [id=" + id + ", ingredient=" + ingredient + ", amount=" + amount + "]";
+	}
+}
---
+++ CoffeeMaker/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/IngredientType.java
@@ -0,0 +1,12 @@
+package edu.ncsu.csc.CoffeeMaker.models.enums;
+
+
+public class IngredientType {
+
+	public final String COFFEE = "Coffee";
+	public final String MILK = "Milk";
+	public final String PUMPKIN_SPICE = "Pumpkin Spice";
+	
+	public enum Ingredients { COFFEE, MILK, PUMPKIN_SPICE }
+	
+}
